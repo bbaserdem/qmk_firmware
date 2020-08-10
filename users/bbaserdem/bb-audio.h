30c63d1b84449cb3e74c83ef817e9d414d509e4a
Batuhan Başerdem <baserdem.batuhan@gmail.com>
2020-08-10 18:41:21 -0400

    Fixed some issues

--- users/bbaserdem/bb-audio.h
+++ users/bbaserdem/bb-audio.h
@@ -20,17 +20,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * Author/Composer:  King Gizzard & the Lizard Wizard
  */
 #define PEOPLE_VULTURES \
-    H__NOTE(_A3), H__NOTE(_G3), E__NOTE(_G3), QD_NOTE(_AS3), H__NOTE(_A3), \
-    H__NOTE(_C4), H__NOTE(_D4), H__NOTE(_AS3), H__NOTE(_A3),
+    H__NOTE(_A3), H__NOTE(_G3), E__NOTE(_G3), QD_NOTE(_AS3),H__NOTE(_A3), \
+    H__NOTE(_C4), H__NOTE(_D4), H__NOTE(_AS3),H__NOTE(_A3),
 
 /* Title:            Wah Wah
  * Author/Composer:  King Gizzard & the Lizard Wizard
  */
 #define WAH_WAH \
-    QD_NOTE(_B2), QD_NOTE(_D3), Q__NOTE(_E3), Q_NOTE(_FS3), \
-    QD_NOTE(_B2), QD_NOTE(_D3), Q__NOTE(_E3), Q_NOTE(_FS3), \
-    S__NOTE(_A3), ED_NOTE(_B3), QD_NOTE(_A3), Q__NOTE(_GS3), Q_NOTE(_A3), \
-    QD_NOTE(_B3), QD_NOTE(_A3), Q__NOTE(_GS3), Q_NOTE(_A3), \
+    QD_NOTE(_B2), QD_NOTE(_D3), Q__NOTE(_E3), Q__NOTE(_FS3), \
+    QD_NOTE(_B2), QD_NOTE(_D3), Q__NOTE(_E3), Q__NOTE(_FS3), \
+    S__NOTE(_A3), ED_NOTE(_B3), QD_NOTE(_A3), Q__NOTE(_GS3),Q__NOTE(_A3), \
+    QD_NOTE(_B3), QD_NOTE(_A3), Q__NOTE(_GS3),Q__NOTE(_A3),
 
 /* Title:            Big Fig Wasp
  * Author/Composer:  King Gizzard & the Lizard Wizard
@@ -67,17 +67,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     E__NOTE(_A3), E__NOTE(_A3), E__NOTE(_B3), E__NOTE(_B3), Q__NOTE(_REST), \
     E__NOTE(_B3), E__NOTE(_B3), E__NOTE(_B3), E__NOTE(_B3), \
     E__NOTE(_B3), E__NOTE(_B3), E__NOTE(_B3), E__NOTE(_B3), \
-    E__NOTE(_DS4), E__NOTE(_DS4), E__NOTE(_DS4), E__NOTE(_D4),
+    E__NOTE(_DS4),E__NOTE(_DS4),E__NOTE(_DS4),E__NOTE(_D4),
 
 // Define the song selections here
-/*
 #define STARTUP_SONG    SONG(HOROLOGY)
 #define GOODBYE_SONG    SONG(PEOPLE_VULTURES)
 #define MUSIC_ON_SONG   SONG(NONAGON_INFINITY)
 #define MUSIC_OFF_SONG  SONG(WAH_WAH)
 #define GAME_ON_SONG    SONG(BIG_FIG_WASP)
 #define GAME_OFF_SONG   SONG(POLYGONDWANALAND)
-*/
+
 // Hook to layer change effects
 layer_state_t layer_state_set_audio(layer_state_t state);
 
